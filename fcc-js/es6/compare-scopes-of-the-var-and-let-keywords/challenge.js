b8dbb5e2dd4c6d91659ec46a2b46bb3ff48c7fc7
unknown <tenifayoabdulsomod@gmail.com>
2023-10-18 08:06:48 +0100

    Add one of the first five for creating PR

--- fcc-js/es6/compare-scopes-of-the-var-and-let-keywords/challenge.js
+++ fcc-js/es6/compare-scopes-of-the-var-and-let-keywords/challenge.js
@@ -1,1 +1,13 @@
-// Solve the challenge in this JavaScript file
+/* eslint-disable prettier/prettier */
+function checkScope() {
+    const i = 'function scope';
+    if (true) {
+      // eslint-disable-next-line no-shadow
+      const i = 'block scope';
+      console.log('function scope i is: ', i);
+    }
+    console.log('block scope i is: ', i);
+    return i;
+  }
+  checkScope();
+  
